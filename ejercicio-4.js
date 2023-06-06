c139e02d275d581be0a345c579aaa7e1d6af7fae
Martin Ballester <mballester@q4tech.com>
2023-06-06 17:30:38 -0300

    ejercicio 4

---
+++ ejercicio-4.js
@@ -0,0 +1,27 @@
+//Ejercicio 4-a
+
+var numeroAleatorio = Math.random();
+if (numeroAleatorio >= 0.5) {
+  alert("Greater than 0.5");
+} else {
+  alert("Lower than 0.5");
+}
+
+//Ejercicio 4-b
+
+var Age = 25;
+if (Age < 2) {
+  alert("Bebe");
+} else if (Age >= 2 && Age <= 12) {
+  alert("Niño");
+} else if (Age >= 13 && Age <= 19) {
+  alert("Adolescente");
+} else if (Age >= 20 && Age <= 30) {
+  alert("Joven");
+} else if (Age >= 31 && Age <= 60) {
+  alert("Adulto");
+} else if (Age >= 61 && Age <= 75) {
+  alert("Adulto mayor");
+} else {
+  alert("Anciano");
+}
