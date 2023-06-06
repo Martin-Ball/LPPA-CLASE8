5d41220a625459a1f59f2a455456897f6e33e3d4
Martin Ballester <mballester@q4tech.com>
2023-06-06 17:22:04 -0300

    ejercicio 1

---
+++ ejercicios.js
@@ -0,0 +1,17 @@
+//Ejercicio 1-a
+var num1 = 5;
+var num2 = 10;
+var suma = num1 + num2;
+console.log(suma);
+
+//Ejercicio 1-b
+var str1 = "Hola";
+var str2 = "Mundo";
+var concatenacion = str1 + " " + str2;
+console.log(concatenacion);
+
+//Ejercicio 1-c
+var str1 = "Hola";
+var str2 = "Mundo";
+var longitudTotal = str1.length + str2.length;
+console.log(longitudTotal);
